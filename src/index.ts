fc56074ac01d7548dc8a0e9afbec7f4194df6869
Joe Hildebrand <joe-github@cursive.net>
2025-02-23 11:10:07 -0700

    Working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,123 @@
+import {Writable} from 'node:stream';
+import {pino} from 'pino';
+import pretty from 'pino-pretty';
+
+export interface LogOptions extends pino.LoggerOptions<never, false> {
+
+  /**
+   * Increase (or decrease if negative) log verbosity by this much.
+   * -3: fatal, -2: error, -1: warn, 0: info, 1: debug, 2: trace.
+   * Useful for -qqqvvvv option processing.
+   */
+  logLevel?: number;
+
+  /** If a string, create this log file and write to it. */
+  logFile?: string | null;
+
+  /** If true, do not log to stdout. */
+  mute?: boolean;
+
+  /** If logFile is set, write synchronously if true (for testing). */
+  sync?: boolean;
+}
+
 /**
- * Foo is a function.
- *
- * @returns The number most between 1 and 3.
+ * "Write" to dev/null.
  */
-export function foo(): number {
-  return 2;
+class WriteSink extends Writable {
+  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
+  public _write(
+    _chunk: any,
+    _encoding: BufferEncoding,
+    callback: (error?: Error | null) => void
+  ): void {
+    callback(null);
+  }
+}
+
+const prettyIgnore = 'pid,hostname,name,host,port';
+
+/**
+ * Create a new log instance.  Mostly useful for testing.
+ *
+ * @param opts Logging options.
+ * @param bindings Extra fields to put into every log item.
+ * @returns Logging object.
+ */
+export function createLog(
+  opts: LogOptions = {},
+  bindings?: pino.Bindings
+): pino.Logger {
+  const {
+    logLevel,
+    logFile,
+    mute = false,
+    sync = false,
+    ...baseOpts
+  } = opts;
+
+  if (logLevel !== undefined) {
+    let levelNum = Math.round(3 - logLevel);
+    if (levelNum < 1) {
+      levelNum = 1;
+    } else if (levelNum > 6) {
+      levelNum = 6;
+    }
+    baseOpts.level = pino.levels.labels[levelNum * 10];
+  }
+
+  let ret: pino.Logger | undefined = undefined;
+  if (mute && !logFile) {
+    // Shh.
+    ret = pino(baseOpts, new WriteSink());
+  } else if (logFile) {
+    const dest = pino.destination({
+      dest: logFile,
+      sync,
+      append: true,
+      mkdir: true,
+    });
+    if (mute) {
+      ret = pino(baseOpts, dest);
+    } else {
+      ret = pino(baseOpts, pino.multistream([{
+        level: baseOpts.level,
+        stream: dest,
+      }, {
+        level: baseOpts.level,
+        stream: pretty({
+          ignore: prettyIgnore,
+        }),
+      }]));
+    }
+  } else {
+    ret = pino(baseOpts, pretty({
+      ignore: prettyIgnore,
+    }));
+  }
+
+  if (bindings) {
+    ret.setBindings(bindings);
+  }
+  return ret;
+}
+
+let instance: pino.Logger | undefined = undefined;
+
+/**
+ * Get a singleton logging object.  Opts and bindings are used the first time,
+ * and ignored thereafter.
+ *
+ * @param opts Logging options.
+ * @param bindings Extra fields to put into every log item.
+ * @returns Singleton logging object.
+ */
+export function getLog(
+  opts: LogOptions,
+  bindings: pino.Bindings = {}
+): pino.Logger {
+  if (!instance) {
+    instance = createLog(opts, bindings);
+  }
+  return instance;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,87 @@
+import {after, test} from 'node:test';
+import {createLog, getLog} from '../lib/index.js';
 import assert from 'node:assert';
-import {foo} from '../lib/index.js';
-import test from 'node:test';
+import fs from 'node:fs/promises';
+import {join} from 'node:path';
+import {promisify} from 'node:util';
+import {streamSym} from 'pino/lib/symbols.js';
+import {tmpdir} from 'node:os';
 
-test('index', () => {
-  assert.equal(foo(), 2);
+const tmp = await fs.mkdtemp(join(tmpdir(), 'cto-af-log-'));
+
+after(async() => {
+  await fs.rm(tmp, {recursive: true});
+});
+
+test('getLog', t => {
+  const a = [];
+  const log = getLog({
+    base: {},
+    timestamp: false,
+  });
+  t.mock.method(log[streamSym], 'write', s => a.push(s));
+  log.info('foo');
+  const log2 = getLog();
+  log2.warn('bar');
+  log2.debug('boo');
+  t.mock.reset();
+  assert.equal(log, log2);
+  assert.deepEqual(a, [
+    '{"level":30,"msg":"foo"}\n',
+    '{"level":40,"msg":"bar"}\n',
+  ]);
+});
+
+test('mute', () => {
+  // Ensure no logs on stdout by inspection
+
+  const log = createLog({
+    mute: true,
+  });
+  log.info('foo');
+});
+
+test('edges', () => {
+  const log = createLog({
+    logLevel: -Infinity,
+  });
+  assert.equal(log.level, 'fatal');
+
+  const log2 = createLog({
+    logLevel: Infinity,
+  });
+  assert.equal(log2.level, 'trace');
+});
+
+test('file', async() => {
+  const logFile = join(tmp, 'file.log');
+  const log = createLog({
+    logFile,
+    mute: true,
+    base: {},
+    timestamp: false,
+    sync: true,
+  });
+  log.info('foo');
+  await promisify(log.flush.bind(log))();
+  const out = await fs.readFile(logFile, 'utf8');
+  assert.equal(out, '{"level":30,"msg":"foo"}\n');
+});
+
+test('both', async t => {
+  const logFile = join(tmp, 'both.log');
+  const log = createLog({
+    logFile,
+    base: {},
+    timestamp: false,
+    sync: true,
+  });
+  const a = [];
+  t.mock.method(log[streamSym], 'write', s => a.push(s));
+  log.info('foo');
+  await promisify(log.flush.bind(log))();
+  t.mock.reset();
+  const out = await fs.readFile(logFile, 'utf8');
+  assert.equal(out, ''); // Because of the write mock.
+  assert.deepEqual(a, ['{"level":30,"msg":"foo"}\n']);
 });
